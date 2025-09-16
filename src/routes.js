759477a389bbef270e59ea055055fde5a13511a5
Dmitri Zagidulin <dzagidulin@gmail.com>
2025-09-15 22:05:19 -0400

    Implement mvp read space.
    Signed-off-by: Dmitri Zagidulin <dzagidulin@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
-import { SPEC_URL } from '../config.default.js';
-import {CreateSpaceRequest} from './requests/CreateSpaceRequest.js';
+import { SPEC_URL } from '../config.default.js'
+import { SpacesRepositoryRequest } from './requests/SpacesRepositoryRequest.js'
+import { SpaceRequest } from './requests/SpaceRequest.js'
 
 export async function initRoutes(app, options) {
   app.get('/', async (request, reply) => {
@@ -8,13 +9,13 @@ export async function initRoutes(app, options) {
 
   // List Spaces
   app.get('/spaces', async (request, reply) => reply.redirect('/spaces/'))
-  app.get('/spaces/', async (request, reply) => {})
+  // app.get('/spaces/', SpacesRepositoryRequest.get)
   // Create a Space
   app.post('/spaces', async (request, reply) => reply.redirect('/spaces/'))
-  app.post('/spaces/', CreateSpaceRequest.post)
+  app.post('/spaces/', SpacesRepositoryRequest.post)
 
   // Get Space info
-  app.get('/space/:spaceId', async (request, reply) => {})
+  app.get('/space/:spaceId', SpaceRequest.get)
 
   app.put('/space/:spaceId', async (request, reply) => {})
   app.delete('/space/:spaceId', async (request, reply) => {})
--- test/server.test.js
+++ test/server.test.js
@@ -26,8 +26,8 @@ describe('API', () => {
     assert.match(body, /specification/)
   })
 
-  describe('Spaces API', () => {
-    it.only('should create space via POST', async () => {
+  describe('Spaces Repository API', () => {
+    it('should create space via POST', async () => {
       const body = {
         "id": "426e7db8-26b5-4fdc-8068-9dcb948fd291",
         "name": "Example space #1",
@@ -40,7 +40,7 @@ describe('API', () => {
         })
       assert.equal(response.status, 201)
       const created = await response.json()
-      assert.partialDeepStrictEqual(created, {
+      assert.deepStrictEqual(created, {
         "id": "426e7db8-26b5-4fdc-8068-9dcb948fd291",
         "name": "Example space #1",
         "type": ["Space"],
@@ -48,6 +48,21 @@ describe('API', () => {
       })
       assert.match(response.headers.get('content-type'), /application\/json/)
       assert.equal(response.headers.get('location'), `/spaces/${body.id}`)
+    })
+
+    it('should read space via GET', async () => {
+      const response = await fetch(
+        new URL('/space/426e7db8-26b5-4fdc-8068-9dcb948fd291', serverUrl)
+      )
+      assert.equal(response.status, 200)
+      assert.match(response.headers.get('content-type'), /application\/json/)
+      const spaceDescription = await response.json()
+      assert.deepStrictEqual(spaceDescription, {
+          id: '426e7db8-26b5-4fdc-8068-9dcb948fd291',
+          type: [ 'Space' ],
+          name: 'Example space #1',
+          controller: 'did:key:z6MkpBMbMaRSv5nsgifRAwEKvHHoiKDMhiAHShTFNmkJNdVW'
+        })
     })
   })
 })
