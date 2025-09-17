04b0b0fae5c4d2fc199a96b4341c485fdca63df1
Dmitri Zagidulin <dzagidulin@gmail.com>
2025-09-16 23:02:25 -0400

    Add onRequest authn hook.
    Signed-off-by: Dmitri Zagidulin <dzagidulin@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,30 @@
-import { SPEC_URL } from '../config.default.js'
 import { SpacesRepositoryRequest } from './requests/SpacesRepositoryRequest.js'
 import { SpaceRequest } from './requests/SpaceRequest.js'
+import { SPEC_URL } from '../config.default.js'
 
-export async function initRoutes(app, options) {
-  app.get('/', async (request, reply) => {
-    return reply.view('home', { title: 'Welcome', SPEC_URL })
+export async function initSpaceRoutes (app, options) {
+  // All SpacesRepository and Space related routes require auth-related headers
+  app.addHook('onRequest', async (request, reply) => {
+    const { headers } = request
+    if (!(headers['authorization'] && headers['capability-invocation'])) {
+      return reply
+        .status(401)
+        .type('application/problem+json')
+        .send({
+          type: `${SPEC_URL}#authorization`,
+          title: 'Invalid request.',
+          errors: [{
+            detail: 'Authorization and Capability-Invocation headers are required.',
+          }]
+        })
+    }
   })
 
   // List Spaces
   app.get('/spaces', async (request, reply) => reply.redirect('/spaces/'))
   // app.get('/spaces/', SpacesRepositoryRequest.get)
+  app.get('/spaces/', async (request, reply) => {})
+
   // Create a Space
   app.post('/spaces', async (request, reply) => reply.redirect('/spaces/'))
   app.post('/spaces/', SpacesRepositoryRequest.post)
